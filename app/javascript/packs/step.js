81241c84c0aaca10b92bda15b5c06fd431b4a2ec
Faizan <hfaizan53@gmail.com>
2022-10-26 02:39:14 +0500

    added homepage project in index route

---
+++ app/javascript/packs/step.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+
+var current_fs, next_fs, previous_fs, fieldset_name; //fieldsets
+var opacity;
+var current = 1;
+var steps = $("fieldset").length;
+var start = $(".fieldsetForm fieldset:first-of-type").data('step');
+
+setProgressBar(current);
+setFieldsetStep(start);
+
+$(".next").click(function(){
+
+    current_fs = $(this).parent();
+    next_fs = $(this).parent().next();
+    fieldset_name = $(next_fs).data('step');
+
+    //$(".fieldsetStep").html(fieldset_name);
+
+    //Add Class Active
+    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+    //show the next fieldset
+    next_fs.show();
+    //hide the current fieldset with style
+    current_fs.animate({opacity: 0}, {
+        step: function(now) {
+            // for making fielset appear animation
+            opacity = 1 - now;
+
+            current_fs.css({
+            'display': 'none',
+            'position': 'relative'
+            });
+            next_fs.css({'opacity': opacity});
+        },
+        duration: 500
+    });
+    setProgressBar(++current);
+    setFieldsetStep(fieldset_name);
+});
+
+$(".previous").click(function(){
+
+    current_fs = $(this).parent();
+    previous_fs = $(this).parent().prev();
+    fieldset_name = $(previous_fs).data('step');
+
+    //Remove class active
+    $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+    //show the previous fieldset
+    previous_fs.show();
+
+    //hide the current fieldset with style
+    current_fs.animate({opacity: 0}, {
+        step: function(now) {
+            // for making fielset appear animation
+            opacity = 1 - now;
+
+            current_fs.css({
+            'display': 'none',
+            'position': 'relative'
+            });
+            previous_fs.css({'opacity': opacity});
+        },
+        duration: 500
+    });
+    setProgressBar(--current);
+    setFieldsetStep(fieldset_name);
+});
+
+function setProgressBar(curStep){
+    var percent = parseFloat(100 / steps) * curStep;
+    percent = percent.toFixed();
+    $(".progress-bar")
+    .css("width",percent+"%")
+}
+
+function setFieldsetStep(name){
+    $(".fieldsetStep span").animate({'opacity': 0}, 250, function () {
+        $(this).text( name );
+    }).animate({'opacity': 1}, 250);
+}
+
+$(".submit").click(function(){
+    return false;
+})
+
+});
