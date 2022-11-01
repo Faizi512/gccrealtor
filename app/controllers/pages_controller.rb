b352264b0605c45ccd387b9878faaeefb3886465
Faizan <hfaizan53@gmail.com>
2022-11-02 03:23:57 +0500

    download attachment

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-    skip_before_action :verify_authenticity_token, only: [:submit_lead]
+    skip_before_action :verify_authenticity_token, only: [:submit_lead, :download_brocher]
 
 
     def index
@@ -31,6 +31,17 @@ class PagesController < ApplicationController
         #     end
         # end
         redirect_to "/thankyou"
+    end
+
+    def download_brocher
+        uri = URI("https://dukeleads.leadbyte.co.uk/api/submit.php?returnjson=yes&campid=GCC-REALTORS&email=#{params[:email]}&firstname=#{params[:first_name]}&lastname=#{params[:last_name]}&phone1=#{params[:phone]}&sid=#{params[:sid]}&project=#{params[:project]}&propertytype=#{params[:propertytype]}&area=#{params[:area]}&purchasepurpose=#{params[:purchasepurpose]}&budget=#{params[:budget]}&phonecode=#{params[:phonecode]}&phonelocal=#{params[:phone]}&sid=#{params[:sid]}&optinurl=#{"https://gccrealtors.com/" + params[:project1]}")
+        res = Net::HTTP.get_response(uri)
+        send_file(
+        "#{Rails.root}/pdf/#{params[:project1]}.pdf",
+        filename: "#{params[:project1]}.pdf",
+        type: "application/pdf",
+        disposition: "attachment"
+        )
     end
     
 end
